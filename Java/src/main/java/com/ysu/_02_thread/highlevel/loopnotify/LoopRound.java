9861cf5c2ef535088281f76b1641e4c3494f19e9
陈宪东 <752890864@qq.com>
2018-11-04 16:38:05 +0800

    [Java] Thread高级. Java循环调度(唤醒)(呼啦圈)的case; 也不知道工作中哪里用得到.

---
+++ Java/src/main/java/com/ysu/_02_thread/highlevel/loopnotify/LoopRound.java
@@ -0,0 +1,75 @@
+package com.ysu._02_thread.highlevel.loopnotify;
+
+import com.ysu._00_common.util.MyStringUtils;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * created by bing57592
+ * 2018-11-04 16:12
+ */
+public class LoopRound {
+
+    private int flag = 1;
+    private Lock lock = new ReentrantLock();
+    private Condition c1 = lock.newCondition();
+    private Condition c2 = lock.newCondition();
+    private Condition c3 = lock.newCondition();
+
+    public void print5(int currentLoop) {
+        lock.lock();
+        try {
+            while (flag != 1) {
+                c1.await();
+            }
+            for (int i = 0; i < 5; i++) {
+                MyStringUtils.print("线程: ? \t打印AA第 ? 次  \t当前第 ? 圈", new Object[]{Thread.currentThread().getName(), i + 1, currentLoop + 1});
+            }
+            flag = 2;
+            c2.signal(); // 唤醒c2
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print10(int currentLoop) {
+        lock.lock();
+        try {
+            while (flag != 2) {
+                c2.await();
+            }
+            for (int i = 0; i < 10; i++) {
+                MyStringUtils.print("线程: ? \t打印AA第 ? 次  \t当前第 ? 圈", new Object[]{Thread.currentThread().getName(), i + 1, currentLoop + 1});
+            }
+            flag = 3;
+            c3.signal(); // 唤醒c3
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print15(int currentLoop) {
+        lock.lock();
+        try {
+            while (flag != 3) {
+                c3.await();
+            }
+            for (int i = 0; i < 15; i++) {
+
+                MyStringUtils.print("线程: ? \t打印AA第 ? 次  \t当前第 ? 圈", new Object[]{Thread.currentThread().getName(), i + 1, currentLoop + 1});
+            }
+            flag = 1;
+            c1.signal(); // 唤醒c1
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ Java/src/main/java/com/ysu/_02_thread/highlevel/loopnotify/Main.java
@@ -0,0 +1,31 @@
+package com.ysu._02_thread.highlevel.loopnotify;
+
+/**
+ * Java多线程循环调用: 实现 A -> B -> C -> A...
+ * 三个线程启动, 要求如下:
+ * AA打印5次, BB打印10次, CC打印15次, 循环20次
+ * created by bing57592
+ * 2018-11-04 16:08
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoopRound loopRound = new LoopRound();
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                loopRound.print5(i);
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                loopRound.print10(i);
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                loopRound.print15(i);
+            }
+        }).start();
+    }
+}
